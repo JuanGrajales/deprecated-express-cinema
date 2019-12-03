497ee539d42a70be377a4332eb14606850f92e75
Juan Grajales <grajales.juanfe11@gmail.com>
2019-12-03 11:30:25 -0500

    create movie model

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+// import Movie model
+const Book = require('../models/Movie'); 
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/', (req, res, next) => {
+  Movie.find()
+  .then((allTheMovies)=>{
+    res.render('movie-views/bunchaBooks', {theBooks: allThebooks});
+  })
+  .catch((err)=>{
+    next(err);
+  })
 });
 
 module.exports = router;
