eef36e46f2d9de22f94f8be69b6d731bd2fe408b
Juan Grajales <grajales.juanfe11@gmail.com>
2019-12-03 11:40:32 -0500

    add form route

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -5,17 +5,18 @@ const Book = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-router.get('/', (req, res, next) => {
   Movie.find()
   .then((allTheMovies)=>{
-    res.render('movie-views/bunchaBooks', {theBooks: allThebooks});
+    res.render('movie-views/allMovies');
   })
   .catch((err)=>{
     next(err);
   })
 });
+
+// Get the page that will show you the form to add a new movie
+router.get('/add-new-movie', (req, res, next)=>{
+  res.render('movie-views/newMovie');
+})
 
 module.exports = router;
